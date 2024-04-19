d3f95f7d6dfba6105db68a99e4cab9b447ac8786
Leticia Corral <leticiacorral@MacBook-Pro-de-Leticia.local>
2024-04-19 14:52:12 -0300

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
+
+print("GOOGLE_CHROME_BIN>>>>>>")
+print(os.environ.get("GOOGLE_CHROME_BIN"))
+print("CHROMEDRIVER_PATH>>>>>>")
+print(os.environ.get("CHROMEDRIVER_PATH"))
+
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
 driver = webdriver.Chrome(
