c4d7d31a6b836e0e51c8931c1a407e7c5af44eb9
Leticia Corral <leticiacorral@MacBook-Pro-de-Leticia.local>
2024-04-19 15:02:12 -0300

    first commit

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from selenium import webdriver
 import os
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+from selenium.webdriver.chrome.service import Service
 
 print("GOOGLE_CHROME_BIN>>>>>>")
-print(os.environ.get("GOOGLE_CHROME_BIN_HEROKU"))
+print(os.environ.get("GOOGLE_CHROME_BIN"))
 print("CHROMEDRIVER_PATH>>>>>>")
 print(os.environ.get("CHROMEDRIVER_PATH"))
 
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN_HEROKU")
+service = Service(executable_path='./chromedriver.exe')
 
-driver = webdriver.Chrome(
-    executable_path=os.environ.get("CHROMEDRIVER_PATH"),
-    chrome_options=chrome_options
-)
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("https://medium.com")
 
