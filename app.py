8ee52159eaedd5f6b2a0fca1c50bc48488d11d7e
Leticia Corral <leticiacorral@MacBook-Pro-de-Leticia.local>
2024-04-19 14:56:19 -0300

    first commit

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
 print("GOOGLE_CHROME_BIN>>>>>>")
-print(os.environ.get("GOOGLE_CHROME_BIN"))
+print(os.environ.get("GOOGLE_CHROME_BIN_HEROKU"))
 print("CHROMEDRIVER_PATH>>>>>>")
 print(os.environ.get("CHROMEDRIVER_PATH"))
 
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN_HEROKU")
 
 driver = webdriver.Chrome(
     executable_path=os.environ.get("CHROMEDRIVER_PATH"),
